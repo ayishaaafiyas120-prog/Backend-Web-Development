fae49f715e79d38562a42c9bd6cf0397e753c925
ayishaaafiyas120-prog <ayisha.aafiya.s.120@kalvium.community>
2026-09-11 09:55:19 +0530

    Changes comment

--- Module 3/Backend Data Flow/services/commentsService.js
+++ Module 3/Backend Data Flow/services/commentsService.js
@@ -13,5 +13,5 @@ const AppError = require('./../utils/AppError');
  * No write may happen before both checks pass.
  */
 exports.addComment = async (postId, userId, body) => {
-  throw new AppError('addComment is not implemented yet', 501);
+  // throw new AppError('addComment is not implemented yet', 501);
 };
